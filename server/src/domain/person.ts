7b06ed4d6801a3f3cbc4d400ba010d300745c1c5
Eduardo Backes <e.backes2011@gmail.com>
2021-01-15 10:44:28 -0300

    feat: aniversary calculator

--- server/src/domain/person.ts
+++ server/src/domain/person.ts
@@ -1,16 +1,3 @@
-import { isAfter, set } from 'date-fns';
-
 export class Person {
-  constructor(private name: string, private birthday: Date) {}
-
-  get nextAniversary(): Date {
-    const now = new Date();
-    const aniversary = set(this.birthday, { year: now.getFullYear() });
-
-    if (isAfter(now, aniversary)) {
-      return set(this.birthday, { year: now.getFullYear() + 1 });
-    }
-
-    return aniversary;
-  }
+  constructor(public name: string, public birthday: Date) {}
 }
---
+++ server/src/next-aniversary-calculator.ts
@@ -0,0 +1,18 @@
+import { isAfter, set } from 'date-fns';
+import { Person } from './domain/person';
+import { ICalendar } from './interfaces/calendar';
+
+export class NextAniversaryCalculator {
+  constructor(private calendar: ICalendar) {}
+
+  exec(person: Person): Date {
+    const now = this.calendar.now();
+    const aniversary = set(person.birthday, { year: now.getFullYear() });
+
+    if (isAfter(now, aniversary)) {
+      return set(person.birthday, { year: now.getFullYear() + 1 });
+    }
+
+    return aniversary;
+  }
+}
